79c1c0652c060671c23d88a649f8fe2448e34969
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2020-11-25 09:40:39 -0500

    fixed binding of this

--- src/Components/App.js
+++ src/Components/App.js
@@ -3,24 +3,24 @@ import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    axios
-      .get('https://api.unsplash.com/search/photos', {
-        params: { query: term },
-        headers: {
-          Authorization:
-            'Client-ID Cc6UuT1dUBcJuzk1Is25bOiRgd0jarXmvcfKgA0JegA',
-        },
-      })
-      .then((response) => {
-        console.log(response.data.results);
-      });
+  state = { images: [] };
+
+  async onSearchSubmit(term) {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization: 'Client-ID Cc6UuT1dUBcJuzk1Is25bOiRgd0jarXmvcfKgA0JegA',
+      },
+    });
+
+    this.setState({ images: response.data.results });
   }
 
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
-        <SearchBar onSubmit={this.onSearchSubmit} />
+        <SearchBar onSubmit={(term) => this.onSearchSubmit(term)} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
