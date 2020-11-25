ee6f81abcdcba4ed681bcc8d85d4066cffd9afe4
Ernesto Ramos <65619655+ernie-ramos@users.noreply.github.com>
2020-11-25 09:49:12 -0500

    refactored unsplash

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,16 +1,13 @@
 import React from 'react';
-import axios from 'axios';
 import SearchBar from './SearchBar';
+import unsplash from '../api/unsplash';
 
 class App extends React.Component {
   state = { images: [] };
 
   onSearchSubmit = async (term) => {
-    const response = await axios.get('https://api.unsplash.com/search/photos', {
+    const response = await unsplash.get('/search/photos', {
       params: { query: term },
-      headers: {
-        Authorization: 'Client-ID Cc6UuT1dUBcJuzk1Is25bOiRgd0jarXmvcfKgA0JegA',
-      },
     });
 
     this.setState({ images: response.data.results });
